5b2566164f842b8a1e9d8736ca7fe6d276b7c636
Meghraj08 <77836923+Meghraj08@users.noreply.github.com>
2021-02-03 17:17:25 +0530

    Update FileHandling.cpp

--- FileHandling.cpp
+++ FileHandling.cpp
@@ -10,15 +10,16 @@ ofstream -> writer
 
 ***/
 
-
-main(){
+main()
+{
     char filedata;
     ofstream obj("sumit.txt");
     obj<<"Hello World\n";
     obj.close();
 
     ifstream obj2("filehandling.cpp");
-    while(obj2){
+    while(obj2)
+    {
         obj2.get(filedata);
         cout<<filedata;
     }
